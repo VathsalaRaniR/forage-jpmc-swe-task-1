b1117ef9aab1887c501f20be4d956f9636d66095
vathsalaranir <varani1903@gmail.com>
2023-06-22 19:04:06 +0530

    task 1

--- client3.py
+++ client3.py
@@ -52,7 +52,6 @@ if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
         """ ----------- Update to get the ratio --------------- """
         prices = {}
         for quote in quotes:
@@ -60,4 +59,4 @@ if __name__ == "__main__":
             prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
